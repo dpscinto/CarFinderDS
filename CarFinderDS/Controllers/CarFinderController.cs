294ec3cc749d75f8565e63a896a0f1a383f7b3ba
dpscinto <dpscinto@gmail.com>
2016-01-15 09:57:11 -0500

    Updated Initial By Make

--- CarFinderDS/Controllers/CarFinderController.cs
+++ CarFinderDS/Controllers/CarFinderController.cs
@@ -13,55 +13,55 @@ namespace CarFinderDS.Controllers
     {
             private ApplicationDbContext db = new ApplicationDbContext();
 
-            public IHttpActionResult GetYears()
+            public IHttpActionResult GetMakes()
             {
             var retval = db.Database.SqlQuery<string>(
-            "EXEC GetYears"
+            "EXEC GetMakes"
             ).ToList();
 
             return Ok(retval);
             }
 
-            public IHttpActionResult GetMakesByYear(string model_year)
+            public IHttpActionResult GetModelByMake(string make)
             {
-                var Smodel_year = new SqlParameter("@model_year", model_year);
+                var Smake = new SqlParameter("@make", make);
                 var retval = db.Database.SqlQuery<string>(
-                "EXEC GetMakesByYear @model_year", Smodel_year
+                "EXEC GetModelByMake @make", Smake
                 ).ToList();
 
                 return Ok(retval);
             }
 
-            public IHttpActionResult GetModelByYearMake(string model_year, string make)
+            public IHttpActionResult GetTrimByMakeModel(string make, string model_name)
             {
-                var Smodel_year = new SqlParameter("@model_year", model_year);
-                var Smake = new SqlParameter("@make", make);
-                var retval = db.Database.SqlQuery<string>(
-                "EXEC GetModelByYearMake @model_year, @make", Smodel_year, Smake
-                ).ToList();
-
-            return Ok(retval);
-            }
-
-            public IHttpActionResult GetTrimByYearMakeModel(string model_year, string make, string model_name)
-            {
-                var Smodel_year = new SqlParameter("@model_year", model_year);
                 var Smake = new SqlParameter("@make", make);
                 var Smodel_name = new SqlParameter("@model_name", model_name);
                 var retval = db.Database.SqlQuery<string>(
-                "EXEC GetTrimByYearMakeModel @model_year, @make, @model_name", Smodel_year, Smake, Smodel_name
+                "EXEC GetTrimByMakeModel @make, @model_name", Smake, Smodel_name
                 ).ToList();
 
             return Ok(retval);
             }
-            public IHttpActionResult GetCars(string model_year="", string make="", string model_name="", string model_trim="")
+
+            public IHttpActionResult GetYearByMakeModelTrim(string make, string model_name, string model_trim)
             {
-                var Smodel_year = new SqlParameter("@model_year", model_year??"");
+                var Smake = new SqlParameter("@make", make);
+                var Smodel_name = new SqlParameter("@model_name", model_name);
+                var Smodel_trim = new SqlParameter("@model_trim", model_trim);
+                var retval = db.Database.SqlQuery<string>(
+                "EXEC GetYearByMakeModelTrim @make, @model_name, @model_trim", Smake, Smodel_name, Smodel_trim
+                ).ToList();
+
+            return Ok(retval);
+            }
+            public IHttpActionResult GetCarsAlt(string make="", string model_name="", string model_trim="", string model_year="")
+            {        
                 var Smake = new SqlParameter("@make", make??"");
                 var Smodel_name = new SqlParameter("@model_name", model_name??"");
                 var Smodel_trim = new SqlParameter("@model_trim", model_trim??"");
+                var Smodel_year = new SqlParameter("@model_year", model_year ?? "");
                 var retval = db.Database.SqlQuery<Car>(
-                "EXEC GetCars @model_year, @make, @model_name, @model_trim", Smodel_year, Smake, Smodel_name, Smodel_trim
+                "EXEC GetCarsAlt @make, @model_name, @model_trim, @model_year", Smake, Smodel_name, Smodel_trim, Smodel_year
                 ).ToList();
 
             return Ok(retval);
